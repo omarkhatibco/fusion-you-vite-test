a2c6b394462eaa9e04ec331a27335c0c4fcd1f8c
Omar Khatib <omar.khatib@tourlane.com>
2023-09-12 23:48:31 +0200

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import { Button } from '@tourlane/fusion-you';
+import { Box } from 'fusion-engine/jsx';
+
 function App() {
-  return <>hello</>;
+  return (
+    <>
+      <Button>Hello</Button>
+      <Box>Hello</Box>
+    </>
+  );
 }
 
 export default App;
